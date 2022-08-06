bdf53c5819b4d265d59f6bb2a93af8b71b423650
Alejandra Gonzales <69376514+alegita@users.noreply.github.com>
2022-08-05 22:32:23 -0400

    reading Excel file

--- formExcel.py
+++ formExcel.py
@@ -6,3 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+
+with open('FormTestExcel.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
