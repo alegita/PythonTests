dc7a0ba6f48957128c5c0cfa544143655c6accd6
Alejandra Gonzales <69376514+alegita@users.noreply.github.com>
2022-08-05 22:25:39 -0400

    Excel

---
+++ formExcel.py
@@ -0,0 +1,8 @@
+# Imports
+import csv
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
